1e595da846e0f65f2c8b47d7cd75f345f4979e9d
meysamcoding <meysamnajafi67@yahoo.com>
2021-06-16 16:48:14 -0700

    save file

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,5 +2,5 @@ USE burgers_db;
 INSERT INTO burger (burger_name, devoured) VALUES ('Cheesburger', false);
 INSERT INTO burger (burger_name, devoured) VALUES ('Hamburger',false);
 INSERT INTO burger (burger_name, devoured) VALUES ('Double burger', true);
-INSERT INTO burger (burger_name, devoured) VALUES ('Chili burger burger', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Meysam burger burger', true);
  
